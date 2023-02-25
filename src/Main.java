5ac0c535610610f7876c96e813b7ec9732fee795
Mark Owings <owingsmk@mail.uc.edu>
2023-02-25 15:31:34 -0500

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 import java.util.Scanner; //Import to activate scanner function
 
-public class TempCalculatorScrip {
+public class Main {
     public static void main(String[] args) {
 
         //Project init
 
-        Scanner celius = new Scanner(System.in); // Scanner function for the variables
-        System.out.print("Enter a temperature in Celsius "); //Output for the user to inter information
-        double C = celius.nextDouble(); //Variable for celcius for the scanner
-        double fahrenheitFinal = ((C*9/5)+32); // the formula to convert Celius to Fahrenheit
+        Scanner fahrenheit = new Scanner(System.in); // Scanner function for the variables
+        System.out.print("Enter a temperature in fahrenheit "); //Output for the user to inter information
+        double f = fahrenheit.nextDouble(); //Variable for Fahrenheit for the scanner
+        double celsiusFinal = ((f-32)*5/9); // the formula to convert Fahrenheit to Celius
 
-        System.out.println("Your temperature in Fahrenheit is " + fahrenheitFinal); //The final output of the program
+        System.out.println("Your temperature in Celsius is " + celsiusFinal); //The final output of the program
     }
-
-
 }
