6122cf3e243d966bc2683acfda56051d684c0787
Mark Owings <owingsmk@mail.uc.edu>
2023-02-25 16:01:12 -0500

    second Commit

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,13 @@ public class Main {
         //Project init
 
         Scanner fahrenheit = new Scanner(System.in); // Scanner function for the variables
+
         System.out.print("Enter a temperature in fahrenheit "); //Output for the user to inter information
         double f = fahrenheit.nextDouble(); //Variable for Fahrenheit for the scanner
         double celsiusFinal = ((f-32)*5/9); // the formula to convert Fahrenheit to Celius
 
         System.out.println("Your temperature in Celsius is " + celsiusFinal); //The final output of the program
+
+        fahrenheit.close();
     }
 }
